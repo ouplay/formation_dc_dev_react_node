b6f80689a7a0b451ca3832a4f4f0714149ce764a
César Fraisseix <cesar.fraisseix@gmail.com>
2021-04-01 16:53:14 +0200

    Initialisation de la todo vace les données de l'api et préparation de la requête post

--- node_src/app.js
+++ node_src/app.js
@@ -5,24 +5,40 @@ const app = express()
 let connect = require("./connection.js")
 let config = require("./config.js")
 
+const cors = require('cors');
+
+const corsOptions = {
+  origin: ['http://localhost:3000'],
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions));
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 
 })
 
-app.get('/todo', async (req, res) => {
+app.get('/tasks', async (req, res) => {
 
   let {db_client, db_connection} = await connect()
   
-  db_connection.collection('todo').find({}).toArray((err, result) => {
+  db_connection.collection('tasks').find({}).toArray((err, result) => {
     if(err) return console.log(err)
 
-    console.log('todo :', result)
+    console.log('tasks :', result)
 
     db_client.close()
     res.send(result)
    
   })
+})
+
+app.post('/task', (req, res) => {
+  
+  //Définir un appelle à la bdd qui ajoute une tache
+  
+
 })
 
 
--- react_src/src/components/TodoList.js
+++ react_src/src/components/TodoList.js
@@ -1,23 +1,48 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Button from "./Button";
 import Input from "./Input";
 import TodoItem from "./TodoItem";
+import axios from "axios";
+
 
 import './TodoList.css'
 
 const TodoList = (props) => {
-  const [tasks, setTasks] = useState([
-    { title: "Faire les courses", done: false },
-    { title: "Sortir le chien", done: true },
-  ]);
+
+
+
+
+
+  const [tasks, setTasks] = useState([]);
   const [inputValue, setInputValue] = useState("");
+
+  useEffect(() => {
+    //Ici on va récupérer les taches sur l'api et les insérer dans notre state React.
+
+    console.log("Lancement de la requête");
+    axios.get("http://localhost:3001/tasks").then((response) => {
+      console.log("Je suis dans le then");
+      console.log(response.data);
+      setTasks(response.data);
+    }).catch((err) => {
+      console.log("error", err)
+    })
+
+    console.log("Je suis après le then");
+    
+
+
+  }, [])
 
   console.log("tasks : ", tasks);
   console.log("inputValue : ", inputValue);
 
   const handleAdd = () => {
-    let newTasks = [...tasks, { title: inputValue, done: false }];
+    let newTask = { title: inputValue, done: false }
+    let newTasks = [...tasks, newTask ];
     setTasks(newTasks);
+    //Faire une appel api (axios) en POST sur /tasks avec les infos de la tache
+    axios.post('http://localhost:3001/tasks', newTask);
     setInputValue("");
   };
 
